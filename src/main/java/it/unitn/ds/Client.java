d5e32e75c6fbb477748bae510337adeab6fcb072
ucesano <ucesano@etik.com>
2026-06-30 16:58:54 +0200

    Client.java: new Akka schedule helper

--- src/main/java/it/unitn/ds/Client.java
+++ src/main/java/it/unitn/ds/Client.java
@@ -4,15 +4,18 @@ import akka.actor.ActorRef;
 import akka.actor.Cancellable;
 import akka.actor.Props;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import scala.concurrent.duration.Duration;
 
 public class Client extends AbstractClient {
 
   private final Map<Integer, Pending> pendingReads = new HashMap<>();
   private final Map<Integer, Pending> pendingWrites = new HashMap<>();
-  private long reqCounter = 0;
+  private final long reqCounter = 0;
 
   Client(long readTimeoutDelay, long writeTimeoutDelay, Optional<ActorRef> defaultTargetReplica, Optional<ActorRef> listener) {
     super(readTimeoutDelay, writeTimeoutDelay, listener, defaultTargetReplica);
@@ -25,6 +28,10 @@ public class Client extends AbstractClient {
   // Props method for automated tests
   public static Props propsWithListener(long readTimeoutDelay, long writeTimeoutDelay, Optional<ActorRef> defaultTargetReplica, ActorRef listener) {
     return Props.create(Client.class, () -> new Client(readTimeoutDelay, writeTimeoutDelay, defaultTargetReplica, Optional.ofNullable(listener)));
+  }
+
+  private Cancellable schedule(long delayMillis, Serializable msg) {
+    return getContext().system().scheduler().scheduleOnce(Duration.create(delayMillis, TimeUnit.MILLISECONDS), getSelf(), msg, getContext().system().dispatcher(), getSelf());
   }
 
   @Override
